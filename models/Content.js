58a7eb97eb9b4d88a82e7cfe840190db9a9de217
saleem4371 <32508556+saleem4371@users.noreply.github.com>
2026-05-14 11:02:41 +0530

    Update Content.js

--- models/Content.js
+++ models/Content.js
@@ -5,9 +5,33 @@ const ContentSchema = new mongoose.Schema(
     hero: Object,
     about: Object,
     contact: Object,
-    branding: Object,
-    services: Array,
-    products: Array
+     branding: {
+      mainLogo: { type: String, default: '' },
+      favicon: { type: String, default: '' },
+    },
+    
+    services: [
+      {
+        id: { type: Number },
+        icon: { type: String, default: '' },
+        title: { type: String, default: '' },
+        desc: { type: String, default: '' },
+        logo: { type: String, default: '' },
+      },
+    ],
+
+    products: [
+      {
+        id: { type: Number },
+        name: { type: String, default: '' },
+        cat: { type: String, default: '' },
+        desc: { type: String, default: '' },
+        url: { type: String, default: '' },
+        tags: { type: String, default: '' },
+        op: { type: String, default: '' },
+        logo: { type: String, default: '' },
+      },
+    ]
   },
   { timestamps: true }
 );
