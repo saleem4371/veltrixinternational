c94f8253f0fb670b4533612780731e4201b0a8bf
saleem4371 <32508556+saleem4371@users.noreply.github.com>
2026-05-14 10:06:18 +0530

    Add files via upload

--- config/db.js
+++ config/db.js
@@ -1,14 +1,20 @@
 const mongoose = require('mongoose');
 
-async function connectDB() {
+const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    if (!process.env.VI_MONGODB_URI) {
+      throw new Error('MONGODB_URI missing');
+    }
+
+    await mongoose.connect(process.env.VI_MONGODB_URI, {
+      serverSelectionTimeoutMS: 30000
+    });
 
     console.log('✅ MongoDB Connected');
   } catch (err) {
-    console.error('MongoDB Error:', err.message);
+    console.error('❌ MongoDB Error:', err.message);
     process.exit(1);
   }
-}
+};
 
 module.exports = connectDB;
